5af288cf6241a35979347cd8f8dbe727e091dd8b
Kyle Corbin <kylecorbin26@gmail.com>
2019-12-16 16:02:02 -0500

    Finished syncCompare

--- database/seeds/users.js
+++ database/seeds/users.js
@@ -1,8 +1,10 @@
+const bcrypt = require('bcryptjs');
+
 
 exports.seed = function(knex) {
       return knex('users').insert([
-        { username: 'Kyle', password: "pass", role_id: 3},
-        { username: 'Joe', password: "pass", role_id: 3},
-        { username: 'Michael', password: "pass", role_id: 3}
+        { username: 'Kyle', password: bcrypt.hashSync('pass', 8), role_id: 3},
+        { username: 'Joe', password: bcrypt.hashSync('pass', 8), role_id: 3},
+        { username: 'Michael', password: bcrypt.hashSync('pass', 8), role_id: 3}
       ]);
 };
